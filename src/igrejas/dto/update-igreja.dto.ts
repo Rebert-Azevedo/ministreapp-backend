e98ddbc8ce56959d3f5dec1114da59df3d81d2b8
Rebert Azevedo <113703728+Rebert-Azevedo@users.noreply.github.com>
2025-10-20 14:08:26 -0300

    Atualização .DTO3

--- src/igrejas/dto/update-igreja.dto.ts
+++ src/igrejas/dto/update-igreja.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateIgrejaDto } from './create-igreja.dto';
 
-export class UpdateEventoDto extends PartialType(CreateIgrejaDto) {}
+export class UpdateIgrejaDto extends PartialType(CreateIgrejaDto) {}
