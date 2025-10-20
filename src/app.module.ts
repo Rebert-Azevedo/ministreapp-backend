fa042e19b595d71aacf8010e3bbebb4b6df1e51f
Rebert Azevedo <113703728+Rebert-Azevedo@users.noreply.github.com>
2025-10-20 12:36:02 -0300

    update .dto2

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { MusicasModule } from './musicas/musicas.module';
 import { ItensInventarioModule } from './itens-inventario/itens-inventario.module';
 import { DespesasModule } from './despesas/despesas.module';
 import { FirebaseModule } from './firebase/firebase.module';
-import { IndisponibilidadeModule } from './indisponibilidade/indisponibilidades.module';
+import { IndisponibilidadesModule } from './indisponibilidades/indisponibilidades.module';
 
 @Module({
   imports: [
@@ -45,7 +45,7 @@ import { IndisponibilidadeModule } from './indisponibilidade/indisponibilidades.
     ItensInventarioModule,
     FirebaseModule,
     DespesasModule,
-    IndisponibilidadeModule,
+    IndisponibilidadesModule,
     ],
   controllers: [AppController],
   providers: [AppService],
